bd34655f70fe83bac90d5cdae0d16f36cdcf12a3
stephengilbert1 <stephengilbert1@gmail.com>
2025-06-10 08:42:08 -0700

    Fixed apostrophes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ export default function AboutPage() {
     <main className="max-w-3xl mx-auto px-6 py-16">
       <h1 className="text-4xl font-bold text-slate-900 mb-4">About Me</h1>
       <p className="text-slate-700 text-lg mb-4">
-        I'm a mechatronics engineer pivoting into software development based in
-        Vancouver, BC.
+        I&apos;m a mechatronics engineer pivoting into software development
+        based in Vancouver, BC.
       </p>
 
       <p className="text-slate-700 text-lg mb-4">
@@ -21,9 +21,10 @@ export default function AboutPage() {
         This portfolio showcases the journey I have been on to sharpen up the
         coding skills I picked up during my engineering degree and build
         tangible projects that solve real world problems. In the Projects page
-        you'll find tools and products that could be applied in the electrical
-        utility industry, where I currently work, as well as tech tools for the
-        recreational activities I love such as skiing and mountain biking.
+        you&apos;ll find tools and products that could be applied in the
+        electrical utility industry, where I currently work, as well as tech
+        tools for the recreational activities I love such as skiing and mountain
+        biking.
       </p>
     </main>
   );
